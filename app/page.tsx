b3402cc45499bcc74e0c9aec5a3d1108b0f51ab9
Green Gem <chris_waitt@me.com>
2024-05-18 12:00:00 +0100

    chore: Update page component with ReactFlow integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
-import Image from "next/image";
+'use client'
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  MiniMap,
+  Controls,
+  Connection
+} from 'reactflow';
 
-export default function Home() {
+import 'reactflow/dist/base.css';
+import CustomNode from '@/components/CustomNode';
+
+import initialNodes from '@/data/initialNodes';
+import initialEdges from '@/data/initialEdges';
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+export default function Page() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback((params: Connection) => setEdges((eds) => addEdge(params, eds)), []);
+
   return (
-    <div></div>
+    <div className='h-dvh w-full'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+        className="bg-teal-50"
+      >
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+    </div>
   );
-}
+};

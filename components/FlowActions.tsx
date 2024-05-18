5d11b06f1c0d19bde78778da3de9a1db50ca7bbd
Green Gem <chris_waitt@me.com>
2024-05-18 16:15:05 +0100

    feat: Add FlowActions component for managing flow actions

---
+++ components/FlowActions.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Button } from "@radix-ui/themes"
+
+function FlowActions() {
+    return (
+        <div className="absolute top-5 left-5 right-5 flex gap-5">
+            <Button className="btn btn-primary">Add Node</Button>
+        </div>
+    )
+}

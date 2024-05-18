012c0b60fc109358ee7a9b40d52e7b8d38bd4bc2
Green Gem <chris_waitt@me.com>
2024-05-18 16:13:43 +0100

    chore: Update layout.tsx to include Radix UI theme and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Theme>{children}</Theme></body>
     </html>
   );
 }

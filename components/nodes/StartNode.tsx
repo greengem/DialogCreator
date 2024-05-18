3e92d63ebfd11e4027206611624d7fb9b1e955b7
Green Gem <chris_waitt@me.com>
2024-05-18 16:14:41 +0100

    refactor: Update StartNode component to include nodeOutput prop

--- components/nodes/StartNode.tsx
+++ components/nodes/StartNode.tsx
@@ -4,9 +4,9 @@ import { NodeTemplate, NodeBody, NodeHeading } from './NodeTemplate';
 
 export default function StartNode() {
   return (
-    <NodeTemplate color='bg-red-500'>
+    <NodeTemplate color='bg-red-500' size='sm'>
         <NodeHeading title='Start' />
-        <NodeBody>It all starts here</NodeBody>
+        <NodeBody nodeOutput>It all starts here</NodeBody>
         <Handle type="source" position={Position.Right} className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-red-500 mr-3 mt-4" />
     </NodeTemplate>
   );

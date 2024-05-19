bf74c7150c63be12435d644585cf8b9296925991
Green Gem <chris_waitt@me.com>
2024-05-19 11:14:06 +0100

    feat: Add ConditionNode and RandomNode components for managing conditions and generating random outputs

--- app/page.tsx
+++ app/page.tsx
@@ -19,14 +19,20 @@ import 'reactflow/dist/base.css';
 import MessageNode from '@/components/nodes/MessageNode';
 import StartNode from '@/components/nodes/StartNode';
 import CharactersNode from '@/components/nodes/CharactersNode';
-import { Button } from "@radix-ui/themes"
+import ConditionNode from '@/components/nodes/ConditionNode';
+import RandomNode from '@/components/nodes/RandomNode';
+
+import { Button } from "@radix-ui/themes";
 import initialNodes from '@/data/initialNodes';
 import initialEdges from '@/data/initialEdges';
+import { IconArrowsShuffle2, IconMessage, IconQuestionMark } from '@tabler/icons-react';
 
 const nodeTypes: NodeTypes = {
   start: StartNode,
   message: MessageNode,
   characters: CharactersNode,
+  condition: ConditionNode,
+  random: RandomNode,
 };
 
 export default function Page() {
@@ -35,10 +41,10 @@ export default function Page() {
 
   const onConnect: OnConnect = useCallback((params: Connection) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
-  const addNode = useCallback(() => {
+  const addNode = useCallback((type: string) => {
     const newNode: Node = {
       id: `${nodes.length + 1}`,
-      type: 'message',
+      type: type,
       position: { x: Math.random() * 800, y: Math.random() * 600 },
       data: {}
     };
@@ -58,7 +64,9 @@ export default function Page() {
           fitView
           className="bg-zinc-900"
         >
-          <Panel position="top-left"><FlowActions addNode={addNode} /></Panel>
+          <Panel position="top-left">
+            <FlowActions addNode={addNode} />
+          </Panel>
           <Background color="#333" variant={BackgroundVariant.Dots} />
           <Controls />
         </ReactFlow>
@@ -67,10 +75,12 @@ export default function Page() {
   );
 }
 
-function FlowActions({ addNode }: { addNode: () => void }) {
+function FlowActions({ addNode }: { addNode: (type: string) => void }) {
   return (
     <div className="flex gap-5">
-      <Button className="btn btn-primary" onClick={addNode}>Add Node</Button>
+      <Button onClick={() => addNode('message')}><IconMessage size={18} />Message</Button>
+      <Button onClick={() => addNode('condition')}><IconQuestionMark size={18} /> Condition</Button>
+      <Button onClick={() => addNode('random')}><IconArrowsShuffle2 size={18} />Random</Button>
     </div>
   );
 }

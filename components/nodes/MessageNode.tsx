a35b6453397c3b05fac5458f72f9e87e88c590a8
Green Gem <chris_waitt@me.com>
2024-05-19 11:13:50 +0100

    refactor: Update MessageNode component to include character selection and message input

--- components/nodes/MessageNode.tsx
+++ components/nodes/MessageNode.tsx
@@ -1,20 +1,28 @@
 'use client'
-import React, { memo, useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import React, { memo, useState, useEffect } from 'react';
+import { Handle, Position, useUpdateNodeInternals } from 'reactflow';
 import { NodeTemplate, NodeBody, NodeHeading } from './NodeTemplate';
 import { Button, Select, TextArea, TextField } from '@radix-ui/themes';
 import useCharacterStore from '@/data/useCharacterStore';
+import { IconX } from '@tabler/icons-react';
 
 interface Choice {
   id: number;
   text: string;
+  handleId: string;
 }
 
-const MessageNode: React.FC = () => {
-  const [choices, setChoices] = useState<Choice[]>([{ id: 1, text: '' }]);
-  
+export function MessageNode({ id }: { id: string }) {
+  const [choices, setChoices] = useState<Choice[]>([]);
+  const updateNodeInternals = useUpdateNodeInternals();
+
   const addChoice = () => {
-    setChoices([...choices, { id: Date.now(), text: '' }]);
+    const newChoice = {
+      id: Date.now(),
+      text: '',
+      handleId: `handle-${Date.now()}`
+    };
+    setChoices([...choices, newChoice]);
   };
 
   const removeChoice = (id: number) => {
@@ -25,36 +33,34 @@ const MessageNode: React.FC = () => {
     setChoices(choices.map(choice => (choice.id === id ? { ...choice, text } : choice)));
   };
 
+  useEffect(() => {
+    updateNodeInternals(id);
+  }, [choices, id, updateNodeInternals]);
+
   return (
     <NodeTemplate color='bg-purple-500' size='xl'>
       <NodeHeading title='Show Message' />
       <NodeBody nodeInput nodeOutput>
         <CharacterSelect />
-        <TextArea placeholder="They walked into a wall.." />
-        <Button onClick={addChoice}>Add Choice</Button>
-        <ul className='flex flex-col gap-y-3'>
-          {choices.map(choice => (
-            <li key={choice.id} className='flex gap-3'>
-              <Button className='shrink-0 grow-0' onClick={() => removeChoice(choice.id)}>X</Button>
-              <TextField.Root 
-                placeholder="Choice.." 
-                className='grow' 
-                value={choice.text} 
-                onChange={(e) => handleTextChange(choice.id, e.target.value)}
-              />
-            </li>
-          ))}
-        </ul>
+        <TextArea placeholder="I'm not arguing, I'm just explaining why I'm right.." size="1" className='nodrag' resize="vertical" />
+        <Choices 
+          choices={choices} 
+          addChoice={addChoice} 
+          removeChoice={removeChoice} 
+          handleTextChange={handleTextChange} 
+        />
       </NodeBody>
       <Handle type="target" position={Position.Left} className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-purple-500 ml-3" />
-      <Handle type="source" position={Position.Right} className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-purple-500 mr-3" />
+      {choices.length === 0 && (
+        <Handle type="source" position={Position.Right} className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-purple-500 mr-3" />
+      )}
     </NodeTemplate>
   );
 }
 
 export default memo(MessageNode);
 
-const CharacterSelect: React.FC = () => {
+function CharacterSelect() {
   const { characters } = useCharacterStore();
 
   return (
@@ -73,3 +79,37 @@ const CharacterSelect: React.FC = () => {
     </Select.Root>
   );
 }
+
+interface ChoicesProps {
+  choices: Choice[];
+  addChoice: () => void;
+  removeChoice: (id: number) => void;
+  handleTextChange: (id: number, text: string) => void;
+}
+
+function Choices({ choices, addChoice, removeChoice, handleTextChange }: ChoicesProps) {
+  return (
+    <>
+      <Button className='bg-purple-500' color='purple' onClick={addChoice}>Add Choice</Button>
+      <ul className='flex flex-col gap-y-3'>
+        {choices.map((choice, index) => (
+          <li key={choice.id} className='flex gap-2 items-center relative'>
+            <Button color='red' className='shrink-0 grow-0 nodrag' onClick={() => removeChoice(choice.id)}><IconX size={16} /></Button>
+            <TextField.Root 
+              placeholder="Choice.." 
+              className='grow nodrag' 
+              value={choice.text} 
+              onChange={(e) => handleTextChange(choice.id, e.target.value)}
+            />
+            <Handle 
+              type="source" 
+              position={Position.Right} 
+              id={choice.handleId} 
+              className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-purple-500 -mr-3"
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};

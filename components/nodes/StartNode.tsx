325ad916f2df45d5809c63fda70b7f15db2aaecd
Green Gem <chris_waitt@me.com>
2024-05-20 17:25:59 +0100

    refactor: Simplify StartNode component in NodeTemplate

--- components/nodes/StartNode.tsx
+++ components/nodes/StartNode.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { Handle, Position,} from 'reactflow';
 import { NodeTemplate, NodeBody, NodeHeading } from './NodeTemplate';
+import { CustomHandle } from '../CustomHandle';
 
 export default function StartNode() {
   return (
     <NodeTemplate color='bg-red-500' size='sm'>
         <NodeHeading title='Start' />
-        <NodeBody nodeOutput>It all starts here</NodeBody>
-        <Handle type="source" position={Position.Right} className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-red-500 mr-3 mt-4" />
+        <NodeBody>It all starts here</NodeBody>
+        <CustomHandle type='source' position={Position.Right} ringColor='ring-red-500' />
     </NodeTemplate>
   );
 }

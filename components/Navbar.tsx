1837c580d88b81d656f71a32b34c6c2987e6ff5c
Green Gem <chris_waitt@me.com>
2024-05-20 17:25:25 +0100

    feat: Add Navbar component for navigation
    The Navbar component was added to the components directory. This change enhances the user experience by providing a consistent navigation across the application.

---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav className="h-16 bg-zinc-950 flex  items-center px-5 border-b-2 border-pink-500 justify-between text-white tracking-tight">
+            <p className="text-2xl font-light">DialogueDesigner</p>
+            <p className="text-zinc-400">Chris Waitt</p>
+        </nav>
+    )
+}

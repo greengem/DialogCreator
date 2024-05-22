e3694382860fe1106332cfce69dcb1d87981f376
Green Gem <chris_waitt@me.com>
2024-05-22 16:02:35 +0100

    refactor: Update auth.ts to include sign-in and sign-out callbacks
    The auth.ts file was updated to include callbacks for the sign-in and sign-out events. These callbacks handle the logic for signing in and out users using their GitHub account. This enhancement improves the authentication functionality of the application.

--- auth.ts
+++ auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
@@ -14,4 +14,52 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }),
     ],
     session: { strategy: "jwt" },
+    callbacks: {
+        async signIn({ user }) {
+            // Ensure email is a non-null string
+            if (!user.email) {
+                console.error("User email is undefined");
+                return false;
+            }
+
+            return true;
+        },
+    },
+    events: {
+        async createUser({ user }) {
+            // Ensure email is a non-null string
+            if (!user.email) {
+                console.error("User email is undefined");
+                return;
+            }
+
+            // Add records to the database for the first-time user
+            try {
+                const flow = await prisma.flow.create({
+                    data: {
+                        user: {
+                            connect: {
+                                email: user.email,
+                            },
+                        },
+                        name: "default-flow",
+                        nodes: {
+                            create: [
+                                {
+                                    type: "characters",
+                                    data: { characters: ["Player", "Narrator"] },
+                                    position: { x: 250, y: 5 },
+                                },
+                            ],
+                        },
+                    },
+                    include: {
+                        nodes: true,
+                    },
+                });
+            } catch (error) {
+                console.error("Error creating flow: ", error);
+            }
+        },
+    },
 });

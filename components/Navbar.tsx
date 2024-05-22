b1e0da219c5bd49383fc5a3158aa226518453d11
Green Gem <chris_waitt@me.com>
2024-05-22 13:56:43 +0100

    feat: Update Navbar to display user avatar and name
    The Navbar component was updated to display the user's avatar and name if available. This feature enhances the personalization of the application by showing the user's identity in the navigation bar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,17 @@
-export default function Navbar() {
+import CustomAvatar from "./CustomAvatar";
+import { auth } from "@/auth";
+
+export default async function Navbar() {
+    const session = await auth();
+    const image = session?.user?.image ?? undefined;
+    const name = session?.user?.id ?? undefined;
+
     return (
-        <nav className="h-16 bg-zinc-950 flex  items-center px-5 border-b-2 border-pink-500 justify-between text-white tracking-tight">
+        <nav className="h-16 bg-zinc-950 flex  items-center px-5 border-b-2 border-zinc-700 justify-between text-white tracking-tight">
             <p className="text-2xl font-light">DialogueDesigner</p>
-            <p className="text-zinc-400">Chris Waitt</p>
+            {session?.user?.email && (
+                <CustomAvatar image={image} name={name} />
+            )}
         </nav>
     )
 }

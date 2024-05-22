ee347ac8c00f84c12d0569174a2087d28a71b1a1
Green Gem <chris_waitt@me.com>
2024-05-22 13:56:51 +0100

    refactor: Update Sidebar component UI
    The Sidebar component UI was updated to improve the layout and functionality. This refactor includes changes such as:
    - Replacing the IconHierarchy component with the IconPlayerPlayFilled component for the start node button.
    - Adding the IconTrash component for the delete flow button.
    - Adjusting the background color, text color, and spacing of the sidebar.
    
    These changes enhance the user experience and provide a more visually appealing sidebar.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,33 +1,26 @@
 'use client'
 import React from 'react';
-import { useNodes } from 'reactflow';
-import { Button } from "@radix-ui/themes";
-import { IconArrowsShuffle2, IconHierarchy, IconMessage, IconPlayerPlay, IconPlus, IconQuestionMark, IconUser } from '@tabler/icons-react';
+import { IconArrowsShuffle2, IconMessage, IconPlayerPlayFilled, IconQuestionMark, IconTrash, IconUser } from '@tabler/icons-react';
 
 type SidebarProps = {
     addNode: (type: string) => void;
+    deleteFlow: () => void;
 };
 
-export default function Sidebar({ addNode }: SidebarProps) {
-  const nodes = useNodes();
+export default function Sidebar({ addNode, deleteFlow }: SidebarProps) {
 
   return (
-    <aside className='w-16 bg-zinc-950 text-pink-500 shadow-md'>
+    <aside className='w-16 bg-zinc-950 text-zinc-400 shadow-md flex flex-col justify-between'>
       <div className='flex flex-col items-center py-5 gap-3'>
-        <IconHierarchy className='text-white' />
+        <button onClick={() => addNode('start')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconPlayerPlayFilled /></button>
         <button onClick={() => addNode('message')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconMessage /></button>
         <button onClick={() => addNode('condition')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconQuestionMark /></button>
         <button onClick={() => addNode('random')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconArrowsShuffle2 /></button>
       </div>
-      {/* <div className='mt-4'>
-        {nodes.map((node) => (
-          <div key={node.id}>
-            Node {node.id} -
-            x: {node.position.x.toFixed(2)},
-            y: {node.position.y.toFixed(2)}
-          </div>
-        ))}
-      </div> */}
+
+      <div className='flex flex-col items-center py-5 gap-3'>
+        <button color='red' onClick={deleteFlow} className='hover:bg-red-700 hover:text-white p-2 rounded-md'><IconTrash /></button>
+      </div>
     </aside>
   );
 }

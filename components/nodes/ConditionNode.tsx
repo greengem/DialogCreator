0e22582e489a398e91801623cdbe08d2242dc70f
Green Gem <chris_waitt@me.com>
2024-05-22 16:01:55 +0100

    refactor: Update ConditionNode component
    The ConditionNode component was refactored to improve code organization and clarity. Changes include:
    - Removed unused import for Handle from 'reactflow'
    - Added CustomHandleNested component import from '../CustomHandle'
    - Updated NodeTemplate color to 'bg-green-700'
    - Updated Handle component to CustomHandleNested component
    - Adjusted styling and layout of the true and false outputs list
    
    These changes enhance the functionality and visual appearance of the ConditionNode component.

--- components/nodes/ConditionNode.tsx
+++ components/nodes/ConditionNode.tsx
@@ -1,34 +1,32 @@
 'use client'
-import { Handle, Position } from 'reactflow';
+import { Position } from 'reactflow';
 import { NodeTemplate, NodeBody, NodeHeading } from './NodeTemplate';
 import { TextField } from '@radix-ui/themes';
-import { CustomHandle } from '../CustomHandle';
+import { CustomHandle, CustomHandleNested } from '../CustomHandle';
+
+function ListItem({ label, id }: { label: string, id: string }) {
+  return (
+    <li className='flex items-center justify-end relative pr-1'>
+      <div className='uppercase'>{label}</div>
+      <CustomHandleNested 
+        type="source" 
+        position={Position.Right} 
+        id={id}
+        ringColor='ring-green-500'
+      />
+    </li>
+  );
+}
 
 export default function ConditionNode() {
   return (
-    <NodeTemplate color='bg-green-500' size='md'>
+    <NodeTemplate color='bg-green-700' size='md'>
       <NodeHeading title='Condition' />
       <NodeBody>
         <TextField.Root placeholder="Has used a teleport..." className='nodrag' />
         <ul className='text-xs flex flex-col gap-3'>
-          <li className='flex items-center justify-end relative pr-3'>
-            <div className='uppercase'>True</div>
-            <Handle 
-              type="source" 
-              position={Position.Right} 
-              id="true-source"
-              className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-green-500" 
-            />
-          </li>
-          <li className='flex items-center justify-end relative pr-3'>
-            <div className='uppercase'>False</div>
-            <Handle 
-              type="source" 
-              position={Position.Right} 
-              id="false-source"
-              className="w-2 h-2 rounded-full !bg-zinc-600 ring-2 ring-green-500" 
-            />
-          </li>
+          <ListItem label='True' id='true-source' />
+          <ListItem label='False' id='false-source' />
         </ul>
       </NodeBody>
       <CustomHandle type='target' position={Position.Left} ringColor='ring-green-500' />

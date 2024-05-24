610abbe0a585893166fe3d5eb146168085db9b58
Green Gem <chris_waitt@me.com>
2024-05-24 11:50:25 +0100

    refactor: Update Sidebar component to include EndNode button
    The Sidebar component was updated to include a button for adding an EndNode to the flow. This change enhances the functionality of the application by providing an easy way for users to add an end point to their flows.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { IconArrowsShuffle2, IconLogin, IconLogout, IconMessage, IconPlayerPlayFilled, IconQuestionMark, IconTrash, IconVariable } from '@tabler/icons-react';
+import { IconArrowsShuffle2, IconLogin, IconLogout, IconMessage, IconPlayerPlayFilled, IconPlayerStopFilled, IconQuestionMark, IconTrash, IconVariable } from '@tabler/icons-react';
 import { signOut, signIn } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
 
@@ -12,9 +12,10 @@ export default function Sidebar({ addNode, deleteFlow }: SidebarProps) {
   const { data: session, status } = useSession();
   
   return (
-    <aside className='w-16 bg-zinc-950 text-zinc-400 shadow-md flex flex-col justify-between'>
+    <aside className='w-16 bg-zinc-900 text-blue-500 shadow-md flex flex-col justify-between'>
       <div className='flex flex-col items-center py-5 gap-3'>
         <button onClick={() => addNode('start')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconPlayerPlayFilled /></button>
+        <button onClick={() => addNode('end')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconPlayerStopFilled /></button>
         <button onClick={() => addNode('message')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconMessage /></button>
         <button onClick={() => addNode('condition')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconQuestionMark /></button>
         <button onClick={() => addNode('random')} className='hover:bg-zinc-700 hover:text-white p-2 rounded-md'><IconArrowsShuffle2 /></button>
